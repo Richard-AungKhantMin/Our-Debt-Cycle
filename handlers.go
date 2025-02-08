1d6d13f7ff5472ecf2669a938b9a62d28b70c22b
cmbigk <bigk89865@gmail.com>
2025-02-08 12:24:56 +0200

    now index.html and homehandler is connected

--- handlers.go
+++ handlers.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func addTransactionHandler(w http.ResponseWriter, r *http.Request) {
@@ -60,4 +63,26 @@ func getBalancesHandler(w http.ResponseWriter, _ *http.Request) {
 func cleanupHandler(w http.ResponseWriter, _ *http.Request) {
 	cleanUpAmounts()
 	w.WriteHeader(http.StatusOK)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		// Define the path to the HTML file (e.g., "./templates/index.html")
+		filePath := filepath.Join("templates", "index.html")
+
+		// Open the HTML file
+		file, err := os.Open(filePath)
+		if err != nil {
+			http.Error(w, "Unable to load the homepage", http.StatusInternalServerError)
+			log.Printf("Error opening file: %v\n", err)
+			return
+		}
+		defer file.Close()
+
+		// Serve the HTML file
+		http.ServeFile(w, r, filePath)
+	} else {
+		// For non-GET methods, send a 405 Method Not Allowed response
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 }
